8bb5e7ab914e80f88e9da2693a149e13a803e51d
Thomas Pope <tpope0928@gmail.com>
2020-04-03 00:21:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,3 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
   loadImgs();
   loadBreedOptions();
 });
+
+function loadImgs() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+    .then(res => res.json())
+    .then(results => {
+      results.message.forEach(image => addImg(image))
+    });
+}
