5e0d4a196059485e21374209361f2153fced7fd1
Thomas Pope <tpope0928@gmail.com>
2020-04-03 00:24:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ let breeds = [];
 document.addEventListener('DOMContentLoaded', function() {
   loadImgs();
   loadBreedOptions();
-});
+}); 
 
 
 function loadImgs() {
@@ -12,7 +12,7 @@ function loadImgs() {
     .then(res => res.json())
     .then(results => {
       results.message.forEach(image => addImg(image))
-    });
+    });  
   }
 
 
@@ -22,56 +22,3 @@ function loadImgs() {
     newDogImg.src = dogImg;
     container.appendChild(newDogImg);
   }
-
-
-  function loadBreedOptions() {
-    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-    fetch(breedUrl)
-      .then(res => res.json())
-      .then(results => {
-
-        breeds = Object.keys(results.message);
-        updateBreedList(breeds);
-        addBreedSelectListener();
-      });
-  }
-
-
-  function updateBreedList(breeds) {
-    let ul = document.querySelector('#dog-breeds');
-    removeChildren(ul);
-    breeds.forEach(breed => addBreed(breed));
-  }
-
-
-  function removeChildren(element) {
-    let child = element.lastElementChild;
-    while (child) {
-      element.removeChild(child);
-      child = element.lastElementChild;
-    }
-  }
-
-  function selectBreedsStartingWith(letter) {
-    updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
-  }
-
-  function addBreedSelectListener() {
-    let breedDropdown = document.querySelector('#breed-dropdown');
-    breedDropdown.addEventListener('change', function (event) {
-      selectBreedsStartingWith(event.target.value);
-    });
-  }
-
-  function addBreed(breed) {
-    let ul = document.querySelector('#dog-breeds');
-    let li = document.createElement('li');
-    li.innerText = breed;
-    li.style.cursor = 'pointer';
-    ul.appendChild(li);
-    li.addEventListener('click', updateColor);
-  }
-
- function updateColor(event) {
-   event.target.style.color = 'palevioletred';
- }
